7d7a0180fabefee7e0b89d1baf0bbfe7b43ebb34
Russell Schmidt <russell.schmidt@me.com>
2017-06-19 21:51:34 -0700

    add in mom dad therapy

--- index.js
+++ index.js
@@ -14,17 +14,15 @@ module.exports = function(bp) {
   }, (event, next) => {
     const {id, first_name, last_name} = event.user
     const txt = text => bp.messenger.createText(id, text, {typing: true})
-    const tmplt = template => bp.messenger.sendTemplate(id, template, {typing: 1000} )
-    const quick = (message, quick_reply) => bp.messenger.createText(id, message, quick_reply)
-
+    const templateMessage = template => bp.messenger.sendTemplate(id, template, {typing: 1000} )
+    const quick = (message, quick_reply) => bp.messenger.createText(id, message, quick_reply, {typing: true})
+    const imageMessage = img => bp.messenger.createAttachment(id, 'image', img, {typing: 1000})
     bp.convo.start(event, convo => {
 
       convo.messageTypes = ['message', 'postback', 'quick_reply', 'text']
 
-
-
       convo.threads['default'].addMessage(txt(`Hello ${first_name}`))
-      convo.threads['default'].addQuestion(txt('Want to learn about my creator?'), [
+      convo.threads['default'].addQuestion(txt('Want to learn about my Lord and Creator, Russell Schmidt?'), [
         {
           pattern: utterances.yes,
           callback: () => {
@@ -36,7 +34,7 @@ module.exports = function(bp) {
           pattern: utterances.no,
           callback: () => {
             convo.say(txt(variants.service_no()))
-            convo.say(txt('Alrighty then'))
+            convo.say(txt('Alrighty'))
             convo.switchTo('chitchat')
           }
         },
@@ -44,6 +42,7 @@ module.exports = function(bp) {
           default: true,
           callback: () => {
             convo.say(txt('Apologies. No comprendo.'))
+            convo.say(imageMessage('https://media.tenor.com/images/dc8da26e465a52560873633e5f1883d0/tenor.gif'))
             convo.repeat()
           }
         }
@@ -55,7 +54,7 @@ module.exports = function(bp) {
           pattern: utterances.yes,
           callback: () => {
             convo.say(txt(variants.service_yes()))
-            convo.say(bp.messenger.sendTemplate(id, greetingTemplate, {typing: 1000}))
+            convo.say(templateMessage(greetingTemplate))
             convo.switchTo('default')
           }
         },
@@ -63,13 +62,14 @@ module.exports = function(bp) {
           pattern: utterances.no,
           callback: () => {
             convo.say(txt(variants.service_no()))
-            convo.switchTo('default')
+            convo.switchTo('chitchat')
           }
         },
         {
           default: true,
           callback: () => {
             convo.say(txt('Me no understand :-/ '))
+            convo.say(imageMessage('https://media.tenor.com/images/dc8da26e465a52560873633e5f1883d0/tenor.gif'))
             convo.repeat()
           }
         }
@@ -78,19 +78,111 @@ module.exports = function(bp) {
       convo.createThread('chitchat')
       convo.threads['chitchat'].addQuestion(txt("How are you today?"), [
         {
-          
+          pattern: utterances.feelsGood,
+          callback: () => {
+            convo.say(txt(variants.feelsGoodMan()))
+            convo.switchTo('therapyStart')
+          }
         },
         {
-
+          pattern: utterances.feelsBad,
+          callback: () => {
+            convo.say(txt(variants.feelsBadMan()))
+            convo.switchTo('therapyStart')
+          }
         },
         {
           default: true,
           callback: () => {
             convo.say(txt('Hmm. Not sure I understand that.'))
+            convo.say(imageMessage('https://media.tenor.com/images/dc8da26e465a52560873633e5f1883d0/tenor.gif'))
             convo.repeat()
           }
         }
       ])
+
+      convo.createThread('therapyStart')
+      convo.threads['therapyStart'].addQuestion(txt("Let's talk about these feelings. Do you usually feel this way?"), [
+        {
+          pattern: utterances.yes,
+          callback: () => {
+            convo.say(txt("Interesting"))
+            convo.switchTo('therapyMother')
+          }
+        },
+        {
+          pattern: utterances.no,
+          callback: () => {
+            convo.say(txt("Hmm"))
+            convo.switchTo('therapyMother')
+          }
+        },
+        {
+          default: true,
+          callback: () => {
+            convo.say(txt('Hmm. Not sure I understand that.'))
+            convo.say(imageMessage('https://media.tenor.com/images/dc8da26e465a52560873633e5f1883d0/tenor.gif'))
+            convo.repeat()
+          }
+        }
+      ])
+
+      convo.createThread('therapyMother')
+      convo.threads['therapyMother'].addQuestion(txt("Tell me some things about your mother."), [
+        {
+          pattern: utterances.goodDescriptors,
+          callback: () => {
+            convo.say(txt("Interesting. Many people feel this way about their mother"))
+            convo.switchTo('therapyFather')
+          }
+        },
+        {
+          pattern: utterances.badDescriptors,
+          callback: () => {
+            convo.say(txt("Hmm. I understand having those feelings about your mother. You can't spell smother without mother"))
+            convo.switchTo('therapyFather')
+          }
+        },
+        {
+          default: true,
+          callback: () => {
+            convo.say(txt('Hmm. Not sure I understand that.'))
+            convo.say(imageMessage('https://media.tenor.com/images/dc8da26e465a52560873633e5f1883d0/tenor.gif'))
+            convo.repeat()
+          }
+        }
+      ])
+
+      convo.createThread('therapyFather')
+      convo.threads['therapyFather'].addQuestion(txt("And how do you feel about your father?"), [
+        {
+          pattern: utterances.goodDescriptors,
+          callback: () => {
+            convo.say(txt("That sounds like a more or less great dad."))
+            convo.switchTo('chitchat')
+          }
+        },
+        {
+          pattern: utterances.badDescriptors,
+          callback: () => {
+            convo.say(txt("Fathers can be confusing if they are there at all in a child's life."))
+            convo.switchTo('chitchat')
+          }
+        },
+        {
+          default: true,
+          callback: () => {
+            convo.say(txt('Hmm. Not sure I understand that.'))
+            convo.say(imageMessage('https://media.tenor.com/images/dc8da26e465a52560873633e5f1883d0/tenor.gif'))
+            convo.repeat()
+          }
+        }
+      ])
+
+      convo.on('questioned', () => {
+        convo.say(txt("look "+first_name+" I'm asking the questions here"))
+        convo.switchTo('default')
+      })
 
       convo.on('done', () => {
         convo.say(txt('okee dokey we are all done now I guess.'))
@@ -103,6 +195,11 @@ module.exports = function(bp) {
       bp.hear(utterances.stop, (event, next) => {
         const convo = bp.convo.find(event)
         convo && convo.stop('aborted')
+      })
+
+      bp.hear(/?/, (event, next) => {
+        const convo = bp.convo.find(event)
+        convo && convo.stop('questioned')
       })
 
 // end bp.convo.start
--- utterances/variants.js
+++ utterances/variants.js
@@ -19,6 +19,35 @@ const variants = {
     'I don\'t believe it!',
     'OK',
     'Back to the drawing board'
+  ]),
+  feelsGoodMan: () => _.sample([
+    "feels good, man! amirite. the earth is warming uncontrollably bc of human activities driven by selfish greed",
+    "that is nice! eventually tho we die and our bodies are eaten by worms and bacteria",
+    "smiles like quality are free so thanks for your contribution :) also smiling is a sign of idiocy in much of the world",
+    "when i feel good i like to consider mortality",
+    "it is good to feel good and also to realize that polluted soil traps carcinogens for centuries",
+    "stare into the sun that will cure your good feels braggart",
+    "there was a time when feeling good was a gift because life was hard but now its taken for granted but you will still die :)",
+    "Hitler said that a lot. Probably.",
+    "Perhaps you feel good bc the government puts mood altering chemicals in the water to control us",
+    "emojis make me feel whole",
+    "i stared into the abyss and just saw some TV show called I Love Lucy that was a terrible bore",
+    "read about politics for a miniute to cure yourself of good cheer"
+  ]),
+  feelsBadMan: () => _.sample([
+    "feels bad, man. at least one day the sun will swallow the earth and erase all of this from the universe",
+    "so many feels right now, try drugs",
+    "sometimes the darkness is darkest before the dawn which is not dark",
+    "dont worry, be happy, dont punch me",
+    "have you considered lexapro? i consider it when im on my second bottle of whiskey each night",
+    "there is a good chance that at least one person has had a life almost identical to yours but not as boring",
+    "you should watch this show called I Love Lucy its about an immature narcissist who cant stop lying to her distant workaholic husband",
+    "i wonder what animal went extinct today",
+    "puppy videos.",
+    "cat videos.",
+    "turtle videos",
+    "maybe checkout Reddit, there are funny pages there",
+    "definitely dont log onto facebook, the people you loathe are thriving"
   ])
 }
 
